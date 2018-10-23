ab090ee15c1385ab9fae391b78f6f53bc5827a1c
Edgar Zanella Alvarenga <e@vaz.io>
2018-10-23 16:56:00 +0200

    change dev config to localhost

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,5 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://192.168.0.4/api/"'
+  BASE_API: '"http://localhost/api/"'
 })
