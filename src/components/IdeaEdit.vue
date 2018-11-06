53360e61ddf6cc2273650dd75900c45a058aa3da
Vincent Ahrend <mail@vincentahrend.com>
2018-11-06 16:14:23 +0100

    Validate IdeaEdit input and notify using snackbar

--- src/components/IdeaEdit.vue
+++ src/components/IdeaEdit.vue
@@ -3,20 +3,24 @@
     <v-container fluid grid-list-md>
       <v-layout row wrap align-center>
         <v-flex md8 offset-md2 xs12>
-          <h1>Deine Idee</h1>
+          <h1>{{ $vuetify.t('$vuetify.IdeaCreation.title') }}</h1>
         </v-flex>
         <v-flex md8 offset-md2 xs12>
           <v-text-field
-            label="Wie soll deine Idee heißen?"
-            hint="z.B. bessere Ausstattung im Computerraum"
-            required
+            name='title'
+            :label="$vuetify.t('$vuetify.IdeaCreation.name')"
+            :hint="$vuetify.t('$vuetify.IdeaCreation.nameExample')"
             v-model="title"
+            v-validate="'required|max:160'"
+            :error-messages="errors.collect('title')"
+            required
             ></v-text-field>
         </v-flex>
         <v-flex md8 offset-md2 xs12>
         <v-textarea
-          label="Was möchtest du vorschlagen?"
-          hint="Hier kanst du deine Idee so ausführlich wie möglich beschreiben..."
+          name='suggestion'
+          :label="$vuetify.t('$vuetify.IdeaCreation.suggestion')"
+          :hint="$vuetify.t('$vuetify.IdeaCreation.suggestionDescription')"
           v-model="description"
           ></v-textarea>
         </v-flex>
@@ -24,6 +28,19 @@
             <v-btn @click="submitIdea" round color="green" dark>{{ $vuetify.t('$vuetify.IdeaCreation.publish') }}</v-btn>
         </v-flex>
       </v-layout>
+      <v-snackbar
+        :value="snackbar !== null"
+        :bottom="true"
+      >
+        {{ snackbar }}
+        <v-btn
+          color="pink"
+          flat
+          @click="snackbar = null"
+        >
+          {{ $vuetify.t('$vuetify.Snackbar.close') }}
+        </v-btn>
+      </v-snackbar>
     </v-container>
   </v-slide-y-transition>
 </template>
@@ -34,46 +51,67 @@ import * as api from '@/api/ideaSpace'
 import Filters from '@/components/Filters'
 
 export default {
+  $_veeValidate: { validator: 'new' },
+
   name: 'IdeaEdit',
   components: { Filters },
   data: () => ({
     title: '',
     description: '',
-    tab: 0
+    tab: 0,
+    snackbar: null,
+    dictionary: {
+      custom: {
+        title: {
+          required: 'Bitte beschreibe hier deine Idee kurz',
+          max: 'Das ist zu lang! Gib hier nur eine kurze Beschreibung deiner Idee ein.'
+        }
+      }
+    }
   }),
 
   props: {
     spaceSlug: ''
   },
 
-  beforeMount: function () {
+  mounted () {
+    this.$validator.localize('en', this.dictionary)
   },
 
   methods: {
     submitIdea: function () {
-      const newIdea = {
-        title: this.title,
-        description: this.description,
-        school_id: this.$store.getters.selected_school,
-        created_by: this.$store.getters.userId,
-        changed_by: this.$store.getters.userId,
-        idea_space: this.$route.params['spaceId']
-      }
+      this.$validator.validate()
+        .then(isFormValid => {
+          // Do nothing if validation fails -  errors are displayed in UI
+          if (!isFormValid) return
 
-      api.createIdea(newIdea)
-        .then((res) => {
-          if (res.status < 400 && res.data.length > 0) {
-            const spaceSlug = this.$route.params['spaceSlug']
-            const ideaId = res.data[0].id
-            this.$router.push(
-              { name: 'IdeaView', params: { spaceSlug, ideaId } }
-            )
-          } else {
-            console.log(res)
+          const newIdea = {
+            title: this.title,
+            description: this.description,
+            school_id: this.$store.getters.selected_school,
+            created_by: this.$store.getters.userId,
+            changed_by: this.$store.getters.userId,
+            idea_space: this.$route.params['spaceId']
           }
+
+          api.createIdea(newIdea)
+            .then((res) => {
+              if (res.status < 400 && res.data.length > 0) {
+                const spaceSlug = this.$route.params['spaceSlug']
+                const ideaId = res.data[0].id
+                this.$router.push(
+                  { name: 'IdeaView', params: { spaceSlug, ideaId } }
+                )
+              } else {
+                this.snackbar = this.$vuetify.t('$vuetify.Snackbar.serverError')
+              }
+            })
+            .catch(() => {
+              this.snackbar = this.$vuetify.t('$vuetify.Snackbar.networkError')
+            })
         })
-        .catch(error => {
-          console.log(error)
+        .catch(() => {
+          this.snackbar = this.$vuetify.t('$vuetify.Snackbar.clientError')
         })
     }
   }
