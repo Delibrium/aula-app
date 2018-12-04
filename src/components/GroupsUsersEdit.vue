1c78e54bf5b871e6bc2c080c560c4f237be11a0d
Vincent Ahrend <mail@vincentahrend.com>
2018-12-04 20:08:26 +0100

    Add CSV upload

--- src/components/GroupsUsersEdit.vue
+++ src/components/GroupsUsersEdit.vue
@@ -1,101 +1,193 @@
 <template>
-  <v-card class='card'>
-    <form>
-      <v-card-title v-if="editingId == null">
-        <h3>{{ $vuetify.t('$vuetify.AdminUsers.titleAdd') }}</h3>
-      </v-card-title>
-      <v-card-title v-else>
-        <h3>{{ $vuetify.t('$vuetify.AdminUsers.titleEdit', user.login) }}</h3>
-      </v-card-title>
-      <v-card-text>
-        <v-text-field
-          name='firstName'
-          v-model='firstName'
-          v-validate="'required'"
-          :error-messages="errors.collect('firstName')"
-          :label="this.$vuetify.t('$vuetify.AdminUsers.formFirstName')"
-          required
-          >
-        </v-text-field>
-        <v-text-field
-          name='lastName'
-          v-model='lastName'
-          v-validate="'required'"
-          :error-messages="errors.collect('lastName')"
-          :label="this.$vuetify.t('$vuetify.AdminUsers.formLastName')"
-          required
-          >
-        </v-text-field>
-        <v-text-field
-          name='username'
-          v-model='username'
-          v-validate=""
-          :error-messages="errors.collect('username')"
-          :label="usernameLabel"
-          >
-        </v-text-field>
-        <v-text-field
-          name='email'
-          v-model='email'
-          v-validate=""
-          :error-messages="errors.collect('email')"
-          :label="this.$vuetify.t('$vuetify.AdminUsers.formEmail')"
-          required
-          >
-        </v-text-field>
-        <v-select
-          name='usergroup'
-          v-if="editingId == null"
-          v-model="usergroup"
-          v-validate="'required'"
-          :error-messages="errors.collect('usergroup')"
-          :items="usergroups"
-          :label="this.$vuetify.t('$vuetify.AdminUsers.formGroup')"
-          required
-        ></v-select>
-        <v-select
-          name='ideaSpace'
-          v-if="editingId == null"
-          v-model="ideaSpace"
-          v-validate=""
-          :error-messages="errors.collect('ideaSpace')"
-          :items="ideaSpaces"
-          :label="this.$vuetify.t('$vuetify.AdminUsers.formIdeaSpace')"
-        ></v-select>
-      </v-card-text>
-      <v-card-actions>
-        <v-btn flat @click="submit" v-if="editingId == null">
-          {{ $vuetify.t('$vuetify.AdminUsers.formCreate') }}
+  <v-card>
+    <v-card-title class="editor-title">
+      <h2 v-if="editingId == null">{{ $vuetify.t('$vuetify.AdminUsers.titleAdd') }}</h2>
+      <h3 v-else>{{ $vuetify.t('$vuetify.AdminUsers.titleEdit', user.login) }}</h3>
+      <v-dialog v-model="csvDialog" max-width="750px">
+        <v-btn slot="activator">
+          <v-icon left>cloud_upload</v-icon>
+          {{ $vuetify.t('$vuetify.AdminUsers.csvDialog') }}
         </v-btn>
-        <v-btn flat @click="submit" v-else>
-          {{ $vuetify.t('$vuetify.AdminUsers.formSave') }}
-        </v-btn>
-        <v-btn flat @click="cancel" v-if="editingId == null">
-          {{ $vuetify.t('$vuetify.AdminUsers.formReset') }}
-        </v-btn>
-        <v-btn flat @click="cancel" v-else>
-          {{ $vuetify.t('$vuetify.AdminUsers.formCancel') }}
-        </v-btn>
-      </v-card-actions>
-    </form>
-    <v-snackbar
-      v-model="showSnackbar"
-      :bottom="true"
-    >
+        <v-card>
+          <v-card-title>
+            <span class="headline">Aus CSV laden</span>
+          </v-card-title>
+
+          <v-card-text>
+            <v-layout row wrap>
+              <v-flex sm12 md6>
+                <p>
+                  Hier können neue Benutzer aus einer Tabellen-Datei erstellt werden.
+                  Die Tabelle sollte im Format CSV (mit Kopfzeile) gespeichert werden und
+                  die Spalten "Vorname" und "Nachname" enthalten. Optional kann
+                  eine Spalte "Email" angegeben werden.
+                </p>
+              </v-flex>
+              <v-flex sm12 md6>
+                <upload-btn
+                  name="csvFile"
+                  class="csv-button"
+                  :disabled="csvUsers != null"
+                  :error-messages="errors.collect('csvFile')"
+                  :title="this.$vuetify.t('$vuetify.AdminUsers.csvFilePicker')"
+                  accept="text/csv*"
+                  :fileChangedCallback="handleFileSelected"
+                ></upload-btn>
+                <v-btn :disabled="csvUsers == null" @click="resetCSVForm">Zurücksetzen</v-btn>
+              </v-flex>
+
+              <v-flex v-if="csvErrors.length > 0">
+                <v-alert v-for="error in csvErrors" :key="error" value="1">
+                  {{ error }}
+                </v-alert>
+              </v-flex>
+
+              <v-flex sm12 mb-3 v-if="csvUsers != null">
+                <v-data-table item-key="username" :headers="csvPreviewHeaders" :items="csvUsers">
+                  <v-toolbar slot="header" class="mb-2" flat>
+                    <v-toolbar-title>Vorschau der {{ this.csvUsers.length }} Nutzer</v-toolbar-title>
+                  </v-toolbar>
+
+                  <template slot="items" slot-scope="props">
+                    <td>{{ props.item.username }}</td>
+                    <td>{{ props.item.first_name }}</td>
+                    <td>{{ props.item.last_name }}</td>
+                    <td>{{ props.item.email }}</td>
+                  </template>
+                </v-data-table>
+              </v-flex>
+
+              <v-flex sm12 md6 px-2 v-if="csvUsers != null">
+                <v-select
+                  name="usergroup"
+                  v-model="usergroup"
+                  v-validate="'required'"
+                  :error-messages="errors.collect('usergroup')"
+                  :items="usergroups"
+                  :label="this.$vuetify.t('$vuetify.AdminUsers.formGroup')"
+                  required
+                ></v-select>
+              </v-flex>
+              <v-flex sm12 md6 px-2 v-if="csvUsers != null">
+                <v-select
+                  name="ideaSpace"
+                  v-model="ideaSpace"
+                  v-validate
+                  :error-messages="errors.collect('ideaSpace')"
+                  :items="ideaSpaces"
+                  :label="this.$vuetify.t('$vuetify.AdminUsers.formIdeaSpace')"
+                ></v-select>
+              </v-flex>
+            </v-layout>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              flat
+              color="primary"
+              @click="saveCSV"
+              :loading="isLoading"
+              :disabled="csvUsers == null">
+              Alle sichern
+            </v-btn>
+            <v-btn
+              flat
+              :disabled="isLoading"
+              @click="closeCSVDialog">
+              Schließen
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-card-title>
+    <v-card-text>
+      <v-text-field
+        name="firstName"
+        v-model="firstName"
+        v-validate="'required'"
+        :error-messages="errors.collect('firstName')"
+        :label="this.$vuetify.t('$vuetify.AdminUsers.formFirstName')"
+        required
+      ></v-text-field>
+      <v-text-field
+        name="lastName"
+        v-model="lastName"
+        v-validate="'required'"
+        :error-messages="errors.collect('lastName')"
+        :label="this.$vuetify.t('$vuetify.AdminUsers.formLastName')"
+        required
+      ></v-text-field>
+      <v-text-field
+        name="username"
+        v-model="username"
+        v-validate
+        :error-messages="errors.collect('username')"
+        :label="usernameLabel"
+      ></v-text-field>
+      <v-text-field
+        name="email"
+        v-model="email"
+        v-validate
+        :error-messages="errors.collect('email')"
+        :label="this.$vuetify.t('$vuetify.AdminUsers.formEmail')"
+        required
+      ></v-text-field>
+      <v-select
+        name="usergroup"
+        v-if="editingId == null"
+        v-model="usergroup"
+        v-validate="'required'"
+        :error-messages="errors.collect('usergroup')"
+        :items="usergroups"
+        :label="this.$vuetify.t('$vuetify.AdminUsers.formGroup')"
+        required
+      ></v-select>
+      <v-select
+        name="ideaSpace"
+        v-if="editingId == null"
+        v-model="ideaSpace"
+        v-validate
+        :error-messages="errors.collect('ideaSpace')"
+        :items="ideaSpaces"
+        :label="this.$vuetify.t('$vuetify.AdminUsers.formIdeaSpace')"
+      ></v-select>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn
+        flat
+        color="primary"
+        @click="submit"
+        v-if="editingId == null"
+      >{{ $vuetify.t('$vuetify.AdminUsers.formCreate') }}</v-btn>
+      <v-btn
+        flat
+        color="primary"
+        @click="submit"
+        v-else
+      >{{ $vuetify.t('$vuetify.AdminUsers.formSave') }}</v-btn>
+      <v-btn
+        flat
+        @click="cancel"
+        v-if="editingId == null"
+      >{{ $vuetify.t('$vuetify.AdminUsers.formReset') }}</v-btn>
+      <v-btn flat @click="cancel" v-else>{{ $vuetify.t('$vuetify.AdminUsers.formCancel') }}</v-btn>
+    </v-card-actions>
+    <v-snackbar v-model="showSnackbar" :bottom="true">
       {{ snackbarMsg }}
       <v-btn
         color="pink"
         flat
         @click="showSnackbar = false"
-      >
-        {{ $vuetify.t('$vuetify.Snackbar.close') }}
-      </v-btn>
+      >{{ $vuetify.t('$vuetify.Snackbar.close') }}</v-btn>
     </v-snackbar>
   </v-card>
 </template>
 
 <script>
 import api from '@/api'
+import UploadButton from 'vuetify-upload-button'
+import Papa from 'papaparse'
 
 export default {
   $_veeValidate: { validator: 'new' },
@@ -127,10 +219,20 @@ export default {
       editingId: null,
       showSnackbar: false,
       snackbarMsg: '',
+      csvDialog: false,
+      csvUsers: null,
+      csvErrors: [],
+      isLoading: false,
       ideaSpaces: [],
       usernameLabel: this.genUsernameLabel(),
       usergroups: defaultGroups,
-      defaultUser: defaultUser
+      defaultUser: defaultUser,
+      csvPreviewHeaders: [
+        { text: 'Benutzername', value: 'username' },
+        { text: 'Vorname', value: 'first_name' },
+        { text: 'Nachname', value: 'last_name' },
+        { text: 'Email', value: 'email' }
+      ]
     }
 
     // The defaultUser value is applied to the data object
@@ -143,19 +245,19 @@ export default {
     this.getIdeaSpaces()
   },
 
+  components: {
+    'upload-btn': UploadButton
+  },
+
   computed: {
-    generatedUsername: function () {
-      // Suggested usernames are a lower case string with two parts:
-      // - the first character of the user's first name - if any
-      // - their last name
-      return (
-        (this.firstName && this.firstName.length > 0 ? this.firstName[0] : '') +
-        this.lastName
-      ).toLowerCase()
+    allUsernames: function () {
+      const existing = this.users == null ? [] : this.users.map(u => u.login)
+      const fromCsv = this.csvUsers == null ? [] : this.csvUsers.map(u => u.username)
+      return existing.concat(fromCsv)
     }
   },
 
-  props: ['handleSuccess', 'handleCancel', 'user'],
+  props: ['handleSuccess', 'handleCancel', 'user', 'users'],
 
   watch: {
     user: function (next, prev) {
@@ -179,6 +281,22 @@ export default {
   },
 
   methods: {
+    genUsername: function (first, last) {
+      let candidate
+      const firstName = first || this.firstName
+      const lastName = last || this.lastName
+
+      // Try prepending characters from the first name to the last name
+      // until a unique username is generated
+      for (let i = 1; i < firstName.length; i++) {
+        candidate = (firstName.slice(0, i) + lastName).toLowerCase()
+        if (!this.allUsernames.includes(candidate)) return candidate
+      }
+
+      // Fallback if no unique username can be generated
+      // random number between 100 and 999
+      return Math.floor((Math.random() * 899) + 100)
+    },
     genUsernameLabel: function () {
       const un = this.username
       const ln = this.lastName
@@ -186,7 +304,7 @@ export default {
       return (un == null || un.length === 0) && ln && ln.length > 1
         ? this.$vuetify.t(
           '$vuetify.AdminUsers.formLoginSuggestion',
-          this.generatedUsername
+          this.genUsername()
         )
         : this.$vuetify.t('$vuetify.AdminUsers.formLogin')
     },
@@ -197,6 +315,15 @@ export default {
       } else {
         this.handleCancel()
       }
+    },
+    resetCSVForm: function () {
+      this.csvUsers = null
+      this.csvErrors = []
+    },
+    closeCSVDialog: function () {
+      this.csvDialog = false
+      this.resetCSVForm()
+      this.cancel()
     },
     getIdeaSpaces: function () {
       const schoolId = this.$store.getters.selected_school
@@ -214,6 +341,104 @@ export default {
         })
       })
     },
+    handleFileSelected: function (selection) {
+      this.resetCSVForm()
+      Papa.parse(selection, {
+        header: true,
+        skipEmptyLines: true,
+        complete: (results) => {
+          if (results.errors.length > 0) {
+            results.errors.map(error =>
+              this.csvErrors.push(
+                `Es gab einen Fehler beim Lesen der Tabelle: ${error.message}`
+              )
+            )
+          }
+
+          const headersComplete = ['Vorname', 'Nachname']
+            .every(field => results.meta.fields.includes(field))
+
+          if (!headersComplete) {
+            this.csvErrors.push(this.$vuetify.t(
+              '$vuetify.AdminUsers.csvFormatError'
+            ))
+            return
+          }
+
+          const entryHasNames = entry =>
+            entry['Vorname'].length > 0 &&
+              entry['Nachname'].length > 0
+
+          if (!results.data.every(entryHasNames)) {
+            this.csvErrors.push(
+              'Einträge konnten z.T. nicht geladen werden, da Vor- oder Nachname fehlen.')
+          }
+
+          this.csvUsers = results.data
+            .filter(entryHasNames)
+            .map(entry => ({
+              first_name: entry['Vorname'],
+              last_name: entry['Nachname'],
+              email: entry['Email'],
+              username: this.genUsername(entry['Vorname'], entry['Nachname'])
+            }))
+        },
+        error: (err) => {
+          this.showSnackbar = true
+          this.snackbarMsg = 'Es gab einen Fehler beim Laden der CSV-Datei.' + err.message
+        }
+      })
+    },
+    saveCSV: function () {
+      if (this.csvUsers.length === 0) {
+        this.snackbarMsg = 'Bitte lade erst eine Tabelle mit Schülern'
+        this.showSnackbar = true
+        return
+      }
+
+      const rolesRequiringIdeaSpace = ['student', 'class_guest', 'moderator']
+      if (rolesRequiringIdeaSpace.includes(this.usergroup) && this.ideaSpace == null) {
+        this.errors.add({
+          field: 'ideaSpace',
+          msg: this.$vuetify.t(
+            '$vuetify.AdminUsers.formRoleRequiresSpace',
+            this.usergroup
+          )
+        })
+        return
+      }
+
+      const usersWithRoles = this.csvUsers.map(user =>
+        Object.assign({}, user, {
+          user_group: this.usergroup,
+          idea_space: this.ideaSpace
+        })
+      )
+
+      this.isLoading = true
+      Promise.all(usersWithRoles.map(api.user.create))
+        .then((responses) => {
+          this.isLoading = false
+          const success = responses.every(res => res.status < 400)
+
+          if (success) {
+            this.showSnackbar = true
+            this.snackbarMsg = this.$vuetify.t(
+              '$vuetify.AdminUsers.snackbarCreated', responses.length)
+            this.closeCSVDialog()
+            this.handleSuccess()
+          } else {
+            this.showSnackbar = true
+            this.snackbarMsg = this.$vuetify.t(
+              '$vuetify.Snackbar.serverError')
+          }
+        })
+        .catch(() => {
+          this.showSnackbar = true
+          this.snackbarMsg = this.$vuetify.t(
+            '$vuetify.Snackbar.clientError')
+        })
+    },
     submit: function () {
       this.$validator.validate()
         .then(isFormValid => {
@@ -228,7 +453,7 @@ export default {
           // character, otherwise default to the autogenerated username
           const username = this.username && this.username.length > 1
             ? this.username
-            : this.generatedUsername
+            : this.genUsername()
 
           const email = this.email && this.email.length > 0
             ? this.email
@@ -262,9 +487,9 @@ export default {
               if (res.status < 400) {
                 this.showSnackbar = true
                 this.snackbarMsg = this.editingId == null
-                  ? this.$vuetify.t('$vuetify.AdminUsers.snackbarCreated')
+                  ? this.$vuetify.t('$vuetify.AdminUsers.snackbarCreated', 1)
                   : this.$vuetify.t('$vuetify.AdminUsers.snackbarSaved')
-                this.handleSuccess(res.data)
+                this.handleSuccess()
               } else {
                 this.showSnackbar = true
                 this.snackbarMsg = this.$vuetify.t(
@@ -293,7 +518,11 @@ export default {
 </script>
 
 <style scoped lang="scss">
-  .card {
-    padding: 10px;
-  }
+.editor-title {
+  justify-content: space-between;
+}
+.csv-button {
+  display: inline;
+  padding-right: 0;
+}
 </style>
