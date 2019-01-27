ee6dfe655624a86aea60752a59532a1218a33030
Edgar Z. Alvarenga <e@vaz.io>
2019-01-27 11:47:03 +0100

    Add breadcrumbs to profile page

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,54 +1,69 @@
 <template>
-  <v-container fluid grid-list-md>
-    <v-slide-y-transition mode="out-in">
-      <v-layout wrap align-center justify-center>
-        <v-flex d-flex md10 xs12 sm12 pa-2 align-content-center justify-center>
-          <v-card>
-            <v-card-text>
-              <v-layout wrap align-center>
-                <v-flex d-flex xs12 sm12 pa-2 align-content-center justify-center>
-                  <v-img :src="user.picture?user.picture:'/static/img/profile_default.png'" max-width="100"/>
-                </v-flex>
-                <v-flex d-flex xs12 sm12 pa-2 align-content-center justify-center>
-                   <h1 class="text-xs-center">{{ $store.getters.user.profile.first_name }} {{ $store.getters.user.profile.last_name }}</h1>
-                </v-flex>
-                <v-flex d-flex xs12 sm12>
-                  <v-layout row wrap align-center justify-center>
-                    <v-flex d-flex sm1 align-content-center justify-center :key="index" v-for="(role, index) in $store.getters.user.profile.role">
-                      <v-chip justify-center class="text-xs-center text-sm-center" label outline color="primary">{{ role }}</v-chip>
-                    </v-flex>
-                  </v-layout>
-                </v-flex>
-                <v-flex>
-                <v-layout wrap align-center justify-center>
-                  <v-flex row d-flex xs12 lg3 pa-2 align-content-center justify-center>
-                   <v-btn round color="primary" dark>{{ $vuetify.t('$vuetify.UserProfile.delegates') }}</v-btn>
-                  </v-flex>
-                  <v-flex row d-flex xs12 lg3 pa-2 align-content-center justify-center>
-                    <v-btn :to="{ name: 'ProfileEdit', params: { userId: $store.getters.userId } }" round color="primary" dark>{{ $vuetify.t('$vuetify.UserProfile.edit') }}</v-btn>
+  <v-container pa-0>
+    <v-layout wrap align-center justify-center>
+      <v-flex md10 xs12>
+         <Breadcrumbs :items="breadcrumbs"/>
+      </v-flex>
+      <v-flex d-flex md10 xs12 sm12 align-content-center justify-center>
+        <v-card>
+          <v-card-text>
+            <v-layout wrap align-center>
+              <v-flex d-flex xs12 sm12 pa-2 align-content-center justify-center>
+                <v-img :src="user.picture?user.picture:'/static/img/profile_default.png'" max-width="100"/>
+              </v-flex>
+              <v-flex d-flex xs12 sm12 pa-2 align-content-center justify-center>
+                 <h1 class="text-xs-center">{{ $store.getters.user.profile.first_name }} {{ $store.getters.user.profile.last_name }}</h1>
+              </v-flex>
+              <v-flex d-flex xs12 sm12>
+                <v-layout row wrap align-center justify-center>
+                  <v-flex d-flex sm1 align-content-center justify-center :key="index" v-for="(role, index) in $store.getters.user.profile.role">
+                    <v-chip justify-center class="text-xs-center text-sm-center" label outline color="primary">{{ role }}</v-chip>
                   </v-flex>
                 </v-layout>
+              </v-flex>
+              <v-flex>
+              <v-layout wrap align-center justify-center>
+                <v-flex row d-flex xs12 lg3 pa-2 align-content-center justify-center>
+                 <v-btn round color="primary" dark>{{ $vuetify.t('$vuetify.UserProfile.delegates') }}</v-btn>
+                </v-flex>
+                <v-flex row d-flex xs12 lg3 pa-2 align-content-center justify-center>
+                  <v-btn :to="{ name: 'ProfileEdit', params: { userId: $store.getters.userId } }" round color="primary" dark>{{ $vuetify.t('$vuetify.UserProfile.edit') }}</v-btn>
                 </v-flex>
               </v-layout>
-            </v-card-text>
-          </v-card>
-        </v-flex>
-      </v-layout>
-    </v-slide-y-transition>
+              </v-flex>
+            </v-layout>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 
 <script>
 
 import api from '@/api'
+import Breadcrumbs from '@/components/Breadcrumbs'
 
 export default {
   name: 'Profile',
   components: {
+    Breadcrumbs
   },
-  data: () => ({
-    user: {}
-  }),
+  data: function () {
+    return {
+      user: {},
+      breadcrumbs: [
+        {
+          title: 'aula',
+          to: '/'
+        },
+        {
+          title: this.$vuetify.t('$vuetify.Idea.idea'),
+          to: this.$route.path
+        }
+      ]
+    }
+  },
 
   beforeMount: function () {
     api.user.get(this.$route.params.userId).then(resp => {
